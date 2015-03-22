a13bd73382b6cd3fd67f2140c535a2a2b3aaa128
James Fargher <james.fargher@powershop.co.nz>
2015-03-23 11:33:28 +1300

    Release v0.2.0

--- oyster.rb
+++ oyster.rb
@@ -2,47 +2,46 @@ require "formula"
 
 class Oyster < Formula
   homepage "https://github.com/proglottis/oyster"
-  head "https://github.com/proglottis/oyster.git"
-  url "https://github.com/proglottis/oyster/archive/v0.1.1.zip"
-  sha1 "c1846d07d7db1715e13b4b1cb72600889935bff4"
+  url "https://github.com/proglottis/oyster/archive/v0.2.0.zip"
+  sha256 "ad1c642ce2401ebb6efe8a6653615e2190c227b7ebb68d10207eccc4ae35ce75"
 
   depends_on "go" => :build
 
   def install
-    ENV["GIT_DIR"] = cached_download/".git" if build.head?
     ENV["GOBIN"] = bin
     ENV["GOPATH"] = buildpath
-    ENV["GOHOME"] = buildpath
-    system "go", "get"
-    system "go", "build", "-o", "oyster"
-    bin.install "oyster"
-    bash_completion.install "oyster_bash_completion.sh"
+    mkdir_p buildpath/"src/github.com/proglottis"
+    ln_s buildpath, buildpath/"src/github.com/proglottis/oyster"
+
+    system "go", "get", "github.com/proglottis/oyster/cmd/oyster", "github.com/proglottis/oyster/cmd/oyster_chrome"
+    system "go", "install", "github.com/proglottis/oyster/cmd/oyster", "github.com/proglottis/oyster/cmd/oyster_chrome"
+
+    bash_completion.install "cmd/oyster/oyster_bash_completion.sh"
+    opt_prefix.install "cmd/oyster_chrome/"
+
+    chrome_native_messaging_path.atomic_write(chrome_native_messaging)
+    chrome_native_messaging_path.chmod 0644
   end
 
-  plist_options :manual => "oyster server"
+  def chrome_native_messaging_name; "com.github.proglottis.oyster.json"; end
+  def chrome_native_messaging_path; prefix + chrome_native_messaging_name; end
 
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/oyster</string>
-          <string>server</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/oyster.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/oyster.log</string>
-      </dict>
-    </plist>
+  def chrome_native_messaging; <<-EOS.undent
+    {
+      "name": "com.github.proglottis.oyster",
+      "description": "Oyster",
+      "path": "#{bin}/oyster_chrome",
+      "type": "stdio",
+      "allowed_origins": [
+        "chrome-extension://knchgkoimfkgfopjfehdkcchmbmkmfgi/"
+      ]
+    }
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    To allow the Google Chrome plugin to connect to Oyster:
+        ln -sfv #{opt_prefix}/#{chrome_native_messaging_name} ~/Library/Application\\ Support/Google/Chrome/NativeMessagingHosts/
     EOS
   end
 
